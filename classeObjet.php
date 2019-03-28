f1197e817ef87473c5f15ec21faa5449efc50705
William DERENNE <derenne.william@gmail.com>
2019-03-28 13:05:36 +0100

    Create classeObjet.php

---
+++ classeObjet.php
@@ -0,0 +1,23 @@
+<?php   header ( 'Content-Type: text/html;charset=UTF-8' );
+
+
+class Objet {
+
+    public $x;
+    
+        /**
+         * CONSTRUCTEUR
+         * 
+         * @param int $_x
+         */
+        public function __construct ( int $_x ) {
+
+            $this -> x = $_x;
+
+        }
+
+    
+    public function getX() {
+        echo '<br />' .  $this -> x . '<br />';
+    }
+}?>
