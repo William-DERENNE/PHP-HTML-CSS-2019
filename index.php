ffbf16b94a1c1727fecb90b11ac6d0bb5d731cf7
William DERENNE <derenne.william@gmail.com>
2019-03-28 13:02:25 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php   header ( 'Content-Type: text/html;charset=UTF-8' );
+    echo 'PHP marche !!!<br />';
+?>
+
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Initiation PHP7-HTML5-CSS3 exercice 2</title>
+        <link rel="stylesheet" href="style.css">
+        <script src="fonctions.js"></script>
+    </head>
+
+    <body>
+
+        
+
+
+    </body>
+</html>
